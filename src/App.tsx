b0b67dc14ac7fad0ea4cc4148537deb3150fd0b4
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2024-12-28 17:47:24 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
-import { Canvas } from '@react-three/fiber';
-import { Suspense } from 'react';
-import { OrbitControls, Stars } from '@react-three/drei';
-import Hero from './components/Hero';
-import Projects from './components/Projects';
-import About from './components/About';
-import Contact from './components/Contact';
+import { Scene } from './components/Canvas/Scene';
+import { Hero } from './components/Hero';
+import { Navigation } from './components/Navigation';
 
 function App() {
   return (
-    <main className="relative min-h-screen">
-      {/* Three.js Canvas */}
-      <Canvas className="absolute top-0 left-0 z-0" camera={{ position: [0, 0, 10] }}>
-        <Suspense fallback={null}>
-          <Stars />
-          <ambientLight intensity={0.5} />
-          <pointLight position={[10, 10, 10]} />
-          <OrbitControls enableZoom={false} enablePan={false} enableRotate={true} />
-        </Suspense>
-      </Canvas>
-
-      {/* Page Content */}
-      <div className="relative z-10">
-        <Hero />
-        <Projects />
-        <About />
-        <Contact />
-      </div>
+    <main className="relative min-h-screen bg-black">
+      <Navigation />
+      <Scene />
+      <Hero />
     </main>
   );
 }
