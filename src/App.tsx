2d21a91ca3bb233555687959383472ce6d329662
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2024-12-28 17:54:26 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
+import { Suspense } from 'react';
 import { Scene } from './components/Canvas/Scene';
 import { Hero } from './components/Hero';
 import { Navigation } from './components/Navigation';
+import { About } from './components/sections/About';
+import { Projects } from './components/sections/Projects';
+import { Contact } from './components/sections/Contact';
+import { ScrollProgress } from './components/layout/ScrollProgress';
 
 function App() {
   return (
-    <main className="relative min-h-screen bg-black">
+    <main className="relative bg-black">
+      <ScrollProgress />
       <Navigation />
-      <Scene />
-      <Hero />
+      
+      {/* Hero Section with 3D Scene */}
+      <section className="h-screen relative">
+        <Suspense fallback={null}>
+          <Scene />
+        </Suspense>
+        <Hero />
+      </section>
+
+      {/* Content Sections */}
+      <About />
+      <Projects />
+      <Contact />
     </main>
   );
 }
